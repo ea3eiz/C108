40603cfcac908478d86ae4729e70edc12e4515f4
manel1954 <ea3eiz@ea3eiz.com>
2022-06-15 12:53:38 +0200

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -4,10 +4,10 @@
                         
                         cd /home/pi
                         sudo rm -R B108
-                        sudo rm -R B1082
+                        sudo rm -R C108
                         git clone http://github.com/ea3eiz/B1082
                         sleep 1
-                        mv B1082 B108
+                        mv C108 B108
                         sleep 1
                         sudo chmod 777 -R B108
                         cd /home/pi/B108
