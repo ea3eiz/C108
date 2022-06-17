fe21b155762b13360571e594d9ea7d49de61d32a
manel1954 <ea3eiz@ea3eiz.com>
2022-06-17 12:37:49 +0200

    1

--- ejecutar_YSF2DMR.sh
+++ ejecutar_YSF2DMR.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd /home/pi/YSF2DMR
-xterm -geometry 77x19+650+659 -bg black -fg white -fa 'Verdana' -fs 9x -T YSF2DMR -e ./YSF2DMR YSF2DMR.ini &
+xterm -geometry 77x7+650+662 -bg black -fg white -fa 'Verdana' -fs 9x -T YSF2DMR -e ./YSF2DMR YSF2DMR.ini &
 sleep 2
 cd /home/pi/MMDVMHost
 
