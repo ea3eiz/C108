c20118b5e201be626aeca69e83e3fa7aad2e7399
manel1954 <ea3eiz@ea3eiz.com>
2022-06-17 12:22:37 +0200

    1

--- ejecutar_DMR2YSF.sh
+++ ejecutar_DMR2YSF.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd /home/pi/DMR2YSF/
-xterm -geometry 77x8+650+659 -bg black -fg white -fa 'Verdana' -fs 9x -T DMR2YSF -e ./DMR2YSF DMR2YSF.ini &
+xterm -geometry 77x7+650+659 -bg black -fg white -fa 'Verdana' -fs 9x -T DMR2YSF -e ./DMR2YSF DMR2YSF.ini &
 sleep 2
 
 cd /home/pi/YSFClients/YSFGateway/
-xterm -geometry 77x14+650+459 -bg black -fg white -fa 'Verdana' -fs 9x -T YSFGATEWAY -e sudo ./YSFGateway YSFGateway.ini &
+xterm -geometry 77x14+650+759 -bg black -fg white -fa 'Verdana' -fs 9x -T YSFGATEWAY -e sudo ./YSFGateway YSFGateway.ini &
 
 cd /home/pi/MMDVMHost
 sudo ./DMR2YSF MMDVMDMR2YSF.ini
