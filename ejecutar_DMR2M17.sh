f2f5ff6f5fda575a54f1bce8e18f93e52a1e4117
manel1954 <ea3eiz@ea3eiz.com>
2022-06-18 20:47:34 +0200

    1

--- ejecutar_DMR2M17.sh
+++ ejecutar_DMR2M17.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
-SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-cd /home/pi/Desktop
-cp ABRIR_DMR2M17.desktop /home/pi
-sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_DMR2M17.sh'" /home/pi/ABRIR_DMR2M17.desktop
-sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICONO_M17_ON.png" /home/pi/ABRIR_DMR2M17.desktop
-sed -i "4c Name[es_ES]=Cerrar DMR2M17" /home/pi/ABRIR_DMR2M17.desktop
-sed -i "21c DMR2M17=ON" /home/pi/status.ini
-cd /home/pi
-cp ABRIR_DMR2M17.desktop /home/pi/Desktop
-
-sudo rm /home/pi/ABRIR_DMR2M17.desktop
-
 
 cd /home/pi/MMDVMHost
-sudo ./MMDVMDMR2M17 MMDVMDMR2M17.ini &
+
+sudo xterm -geometry 77x9+1276+798 -bg black -fg white -fa 'Verdana' -fs 9x -T DMR2M17 -e sudo ./MMDVMDMR2M17 MMDVMDMR2M17.ini &
+
 
 
 cd /home/pi/DMR2M17
-sudo xterm -geometry 88x11+1272+420 -bg black -fg white -fa 'Verdana' -fs 9x -T DMR2M17 -e ./DMR2M17 DMR2M17.ini
+sudo xterm -geometry 77x5+1276+260 -bg black -fg white -fa 'Verdana' -fs 9x -T DMR2M17 -e ./DMR2M17 DMR2M17.ini
 
 
 
