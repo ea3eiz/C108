57e583bb4f5ed3429d97a00b56c0b3d9709dc5ec
manel1954 <ea3eiz@ea3eiz.com>
2022-06-20 17:09:56 +0200

    1

--- ejecutar_m17.sh
+++ ejecutar_m17.sh
@@ -3,7 +3,7 @@
 
             cd /home/pi/mvoice
             ./mvoice
-            else
+    
             #cd /home/pi/B108
             #./aviso
 #zenity --warning --ok-label=CERRAR --title=Sistema_M17 --window-icon=/home/pi/B108/ICONO_DMRGATEWAY_ON.png --width=500 --text "\nYa está disponible la versión ADER B108 (25-10-2020) M17\npuedes descargarla de la web:\nhttp://www.associacioader.com\n\ncon la nueva versión ya puedes usar el Sistema M17."
