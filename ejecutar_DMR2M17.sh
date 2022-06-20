ea71cf6c48d40432d9d0ffae5584c85849b8fa13
manel1954 <ea3eiz@ea3eiz.com>
2022-06-20 22:24:01 +0200

    1

--- ejecutar_DMR2M17.sh
+++ ejecutar_DMR2M17.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+cd /home/pi/MMDVMHost
+sudo xterm -geometry 77x9+1276+798 -bg black -fg white -fa 'Verdana' -fs 9x -T MMTVMDMR2M17 -e sudo ./MMDVMDMR2M17 MMDVMDMR2M17.ini &
+
+sleep 20
+
 cd /home/pi/DMR2M17
 sudo xterm -geometry 77x5+1276+260 -bg black -fg white -fa 'Verdana' -fs 9x -T DMR2M17 -e ./DMR2M17 DMR2M17.ini &
 
-sleep 10
 
-cd /home/pi/MMDVMHost
-sudo xterm -geometry 77x9+1276+798 -bg black -fg white -fa 'Verdana' -fs 9x -T MMTVMDMR2M17 -e sudo ./MMDVMDMR2M17 MMDVMDMR2M17.ini &
+
+
 
 
 
