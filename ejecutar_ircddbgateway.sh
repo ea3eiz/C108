787449d8102b2ceb0e0eb609d81f818ae80ca96b
manel1954 <ea3eiz@ea3eiz.com>
2022-07-01 18:03:18 +0200

    1

--- ejecutar_ircddbgateway.sh
+++ ejecutar_ircddbgateway.sh
@@ -3,6 +3,6 @@
 # Cierra dstar en DVSwitch Server
 sudo systemctl stop ircddbgatewayd
 
-sudo ircddbgateway -gui
+sudo ircddbgateway -gui &
 sudo sed -i '51c IRCDDBGATEWAY=ON' /home/pi/.local/autoarranque.ini
 
