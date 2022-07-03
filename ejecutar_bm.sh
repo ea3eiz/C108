ea55e70dafbfd65b3891938f2242c8219abd04e7
manel1954 <ea3eiz@ea3eiz.com>
2022-07-03 20:03:26 +0200

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -10,7 +10,7 @@
 #sleep 2
 ##/home/pi/B108/./qt_info_bm & sudo ./MMDVMBM MMDVMBM.ini 
 #sudo ./MMDVMBM MMDVMBM.ini
-
+sed -i "50c BM=ON" /home/pi/status.ini
 cd /home/pi/MMDVMHost
 echo "\33[38;5;138m"
 echo " **************************************************************************"
