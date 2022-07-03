0e5078fb84c8736bae9d65cff02e03581cbfd733
manel1954 <ea3eiz@ea3eiz.com>
2022-07-03 19:43:32 +0200

    1

--- ejecutar_bm.sh
+++ ejecutar_bm.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 #sudo killall -9 MMDVMBM
-sudo sed -i "7c MMDVMBM=ON" /home/pi/status.ini
+
+#sudo sed -i "7c MMDVMBM=ON" /home/pi/status.ini
+#cd /home/pi/MMDVMHost
+#echo "\33[38;5;138m"
+#echo " **************************************************************************"
+#echo "                          ABRIENDO BRANDMEISTER                             "
+#echo " **************************************************************************"
+#sleep 2
+##/home/pi/B108/./qt_info_bm & sudo ./MMDVMBM MMDVMBM.ini 
+#sudo ./MMDVMBM MMDVMBM.ini
+
 cd /home/pi/MMDVMHost
 echo "\33[38;5;138m"
 echo " **************************************************************************"
-echo "                          ABRIENDO BRANDMEISTER                             "
+echo "                          ABRIENDO BRANDMEISTER                            "
 echo " **************************************************************************"
 sleep 2
-#/home/pi/B108/./qt_info_bm & sudo ./MMDVMBM MMDVMBM.ini 
+#/home/pi/A108/./qt_info_bm & sudo ./MMDVMBM MMDVMBM.ini
 sudo ./MMDVMBM MMDVMBM.ini
+#sudo ./MMDVMBM MMDVMBM.ini
 
+sed -i "50c BM=ON" /home/pi/status.ini
 
 
